c0c1e3ca8ef5601288aaf0019bf23214b21b2f2e
Aseer KT <aseermiqdad@gmail.com>
2021-06-26 12:44:35 +0530

    trial: dark-mode

--- client/src/App.vue
+++ client/src/App.vue
@@ -62,11 +62,19 @@ export default {
   padding: 0;
 }
 
+:root {
+  --clr-text-primary: #2c3e50;
+}
+
+[data-theme='light'] {
+  --clr-text-primary: #222;
+}
+
 #app {
   font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: var(--clr-text-primary);
 }
 
 .app {
